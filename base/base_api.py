14b7b6e237d57662537e7e9ee6b6e6391e2698fe
Anush Gabrielyan <anushgab97@gmail.com>
2023-03-31 22:20:32 +0400

    configure general functionality for API CRUD operations

--- base/base_api.py
+++ base/base_api.py
@@ -3,7 +3,7 @@ import requests
 
 class BaseApi:
 
-    def get_request(self, url, params=None, headers=None):
+    def get_request(self, url, headers, params=None):
         """
         Use this method to send the get request
         :param url: The request URL
@@ -12,6 +12,44 @@ class BaseApi:
         :return: response
         """
         response = requests.get(url, headers=headers, params=params, verify=False)
+        return response
+
+    def post_request(self, url, json, headers, params=None):
+        """
+        Use this method to send post request
+        :param url: The request URL
+        :param json: Request body format
+        :param params: The request params(OPTIONAL)
+        :param headers: The request headers(OPTIONAL)
+        :return: response
+        """
+
+        response = requests.post(url, data=json, params=params, headers=headers, verify=False)
+        return response
+
+    def put_request(self, url, json, *kwargs):
+
+        """"
+        Use this method to send put request
+        :param url: The request URL
+        :param json: Request body format
+        :param params: The request params(OPTIONAL)
+        :param headers: The request headers(OPTIONAL)
+        :return: response
+        """
+
+        response = requests.put(url, json, params=kwargs[1], headers=kwargs[0], verify=False)
+        return response
+
+    def delete_request(self, url, *kwargs):
+        """
+        Use this method to send delete request
+        :param url: The request URL
+        :param params: The request params(OPTIONAL)
+        :param headers: The request headers(OPTIONAL)
+        :return: response
+        """
+        response = requests.delete(url, headers=kwargs[0], verify=False)
         return response
 
     def check_status_code(self, response, expected_status_code):
@@ -23,3 +61,7 @@ class BaseApi:
         """
 
         return response.status_code == expected_status_code
+
+
+
+
--- endpoints/productservice.py
+++ endpoints/productservice.py
@@ -1,10 +1,24 @@
+import json
+
 from base.base_api import BaseApi
 
 
 class ProductService(BaseApi):
     products_endpoint = '/products'
 
-    def get_products(self, url, token, expected_status_code):
+    def get_products(self, url, token):
         response = self.get_request(url + self.products_endpoint, token)
 
-        return self.check_status_code(response, expected_status_code)
+        return response
+
+    def check_products_data_by_length(self, data):
+        json_data = json.loads(data.text)
+
+        return len(json_data)
+
+    #checking
+    def update_product(self, url, token, id):
+        response = self.put_request(url + self.products_endpoint, {"title": "test product",
+    "price": 13.5,},token, id)
+
+
